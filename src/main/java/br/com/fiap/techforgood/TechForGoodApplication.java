d83719afb3e07820dec4885eb1797df6b9783383
Joao-Pedro <joaopedroprog@outlook.com>
2021-11-13 15:27:56 -0300

    Criado projeto Spring Boot

---
+++ src/main/java/br/com/fiap/techforgood/TechForGoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techforgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechForGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechForGoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/techforgood/TechForGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techforgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechForGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
