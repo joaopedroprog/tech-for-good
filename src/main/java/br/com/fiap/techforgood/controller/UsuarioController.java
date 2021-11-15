0aef8bbbf91e41306fa4d5727fa83940d9aa07e8
Joao-Pedro <joaopedroprog@outlook.com>
2021-11-15 16:58:54 -0300

    Adicionada feature de buscar, atualizar e excluir usuário

--- src/main/java/br/com/fiap/techforgood/controller/UsuarioController.java
+++ src/main/java/br/com/fiap/techforgood/controller/UsuarioController.java
@@ -4,29 +4,57 @@ import br.com.fiap.techforgood.dto.UsuarioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.fiap.techforgood.service.UsuarioService;
 
+import javax.websocket.server.PathParam;
+
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
-	
-	@ResponseBody
-    @PostMapping("/usuario")
+
+    @PostMapping("/cadastrar")
     public ResponseEntity<?> createUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(this.usuarioService.createUsuario(usuarioDTO));
         } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar usuario!");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar usuário!");
         }
         
     }
-    
+
+    @GetMapping("/buscar/{idUsuario}")
+    public ResponseEntity<?> getUsuario(@PathVariable Long idUsuario) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(this.usuarioService.getUsuario(idUsuario));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao buscar usuário");
+        }
+    }
+
+    @PutMapping("/atualizar/{idUsuario}")
+    public ResponseEntity<?> updateUsuario(@PathVariable Long idUsuario, @RequestBody UsuarioDTO usuarioDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(this.usuarioService.updateUsuario(idUsuario, usuarioDTO));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar usuário!");
+        }
+    }
+
+    @DeleteMapping("/excluir/{idUsuario}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable Long idUsuario) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(this.usuarioService.deleteUsuario(idUsuario));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao excluir usuário!");
+        }
+    }
+
 }
